d984793e608c1d49be587613009409f7865a5894
vietnguyen <viet@dhis2.org>
2017-02-06 16:10:46 +0700

    minor fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    config: {
-      src: 'index.html',
-      dist: 'build/'
-    },
     'string-replace': {
       dist: {
         files: {
